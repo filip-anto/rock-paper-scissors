abf177a76c6f5c5992de7cc8d24e268bf98b7c42
filip-anto <filip_anto@hotmail.com>
2022-10-17 11:39:29 +0300

    Make computer choice and convert player choice to same syntax as computer choice

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function getComputerChoice(){
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return ("Rock");
+    }
+    else if (computerChoice === 1) {
+        return ("Paper");
+    }
+    else {
+        return ("Scissors");
+    }
+}
 
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Please enter your choice");
+    playerChoice=playerChoice.toLowerCase();
+    playerChoice=playerChoice.charAt(0).toUpperCase()+playerChoice.slice(1);
+    return (playerChoice);
 }
-function getPlayerChoice(){
-    let playerChoice=prompt("Please enter your choice");
-    return(playerChoice);
-}
+getComputerChoice();
 getPlayerChoice();
