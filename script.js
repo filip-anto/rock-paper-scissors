c8d2dccc3da6db51f0262fe4034752476e239780
filip-anto <filip_anto@hotmail.com>
2022-10-17 12:36:28 +0300

    Make sure input is valid

--- script.js
+++ script.js
@@ -11,38 +11,47 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Please enter your choice");
-    playerChoice=playerChoice.toLowerCase();
-    playerChoice=playerChoice.charAt(0).toUpperCase()+playerChoice.slice(1);
+function capitalize(playerChoice) {
+    console.log (playerChoice);
+    playerChoice = playerChoice.toLowerCase();
+    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
     return (playerChoice);
 }
 
-function playRound(computerChoice,playerChoice){
-    if (computerChoice===playerChoice){
-        return("Draw");
+function getPlayerChoice() {
+    let playerChoice="";
+    while ((!((playerChoice.localeCompare("Rock"))===0))&& (!((playerChoice.localeCompare("Paper"))===0)) &&(!((playerChoice.localeCompare("Scissors"))===0)) ) {
+        playerChoice = prompt("Please enter your choice");
+        playerChoice=capitalize(playerChoice);
     }
-    else if(computerChoice==="Rock" && playerChoice==="Scissors"){
-        return("You lose!"+computerChoice+" beats "+playerChoice);
+    return (playerChoice);
+}
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return ("It's a draw");
     }
-    else if(computerChoice==="Rock" && playerChoice==="Paper"){
-        return("You win!"+playerChoice+" beats "+computerChoice);
+    else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+        return ("You lose!" + computerChoice + " beats " + playerChoice);
     }
-    else if(computerChoice==="Paper" && playerChoice==="Rock"){
-        return("You lose!"+computerChoice+" beats "+playerChoice);
+    else if (computerChoice === "Rock" && playerChoice === "Paper") {
+        return ("You win!" + playerChoice + " beats " + computerChoice);
     }
-    else if(computerChoice==="Paper" && playerChoice==="Scissors"){
-        return("You win!"+playerChoice+" beats "+computerChoice);
+    else if (computerChoice === "Paper" && playerChoice === "Rock") {
+        return ("You lose!" + computerChoice + " beats " + playerChoice);
     }
-    else if(computerChoice==="Scissors" && playerChoice==="Paper"){
-        return("You lose!"+computerChoice+" beats "+playerChoice);
+    else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+        return ("You win!" + playerChoice + " beats " + computerChoice);
     }
-    else if(computerChoice==="Scissors" && playerChoice==="Rock"){
-        return("You win!"+playerChoice+" beats "+computerChoice);
+    else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+        return ("You lose!" + computerChoice + " beats " + playerChoice);
+    }
+    else if (computerChoice === "Scissors" && playerChoice === "Rock") {
+        return ("You win!" + playerChoice + " beats " + computerChoice);
     }
 }
-for (let i=0;i<5;i++){
-let computerChoice=getComputerChoice();
-let playerChoice=getPlayerChoice();
-console.log(playRound(computerChoice,playerChoice));
+for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    console.log(playRound(computerChoice, playerChoice));
 }
